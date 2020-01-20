c3f05f8a68e26ab94c286805382ccfad0fd1421b
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-01-20 16:34:36 +0900

    Update Permutation.py

--- Permutation.py
+++ Permutation.py
@@ -1,6 +1,13 @@
 from itertools import permutations 
+from itertools import combinations 
+  
+comb = combinations([1, 2, 3], 2) 
+  
+# Print the obtained combinations 
+for i in list(comb): 
+    print(i) 
 
 perm = permutations([1, 2, 3]) 
 
 for i in list(perm): 
-    print i
+    print(i)
