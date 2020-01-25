c6dbb28c1a184dab1861581bcf40ba240dfd6ff8
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-01-26 01:15:03 +0900

    Create PrimeNum.py

---
+++ PrimeNum.py
@@ -0,0 +1,18 @@
+def primes_up_to_good(n:int) -> [int]:
+    seive = [False, False] + [True] * (n - 1)
+    for k in range(2, int(n ** 0.5 + 1.5)):
+        if seive[k]:
+            seive[k*2::k] = [False] * ((n - k) // k)
+            return [x for x in range(n+1) if seive[x]]
+           
+def is_primes(num:int) -> bool:
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                print(num,"is not a prime number")
+                print(i,"times",num//i,"is",num)
+                break
+        else:
+            print(num,"is a prime number")
+    else:
+        print(num,"is not a prime number")
